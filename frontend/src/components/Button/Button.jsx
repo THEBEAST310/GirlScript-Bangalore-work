d0f474c4e6fb72fff7e7dd35f7a65a9fef2e5b53
Irshad Ansari <irshadjsr21@gmail.com>
2020-06-14 21:38:55 +0530

    [Feat] Improved button component size; Added in Blog

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -1,10 +1,42 @@
 import React from "react";
 import styles from "./Button.module.scss";
+import { Link } from "react-router-dom";
 
-export default function({ children, onClick }) {
+/*
+ * To open an internal link on button click, add `to` prop as in `Link` tag.
+ *
+ * To open an external link on button click, add `href` prop as in `a` tag.
+ *
+ * You can also pass `onClick` prop on the button 
+ */
+function Button({ children, onClick, to, href }) {
+  if(to) {
+     return (
+      <Link to={to} className={styles["btn"]} onClick={onClick}>
+        {children}
+      </Link>
+     );
+  } 
+
+  if(href) {
+    return (
+      <a 
+        target="_blank" 
+        rel="noreferrer noopener" 
+        href={href} 
+        className={styles["btn"]} 
+        onClick={onClick}
+      >
+        {children}
+      </a>
+    );
+  }
+
   return (
     <button className={styles["btn"]} onClick={onClick}>
       {children}
     </button>
   );
 }
+
+export default Button;
