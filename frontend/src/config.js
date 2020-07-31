256f54a449f219dfa5fddb57bf21efada23739b7
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2020-07-31 16:42:11 +0530

    About Us page

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,4 +3,5 @@ export default {
   blogSheetId: process.env.REACT_APP_BLOG_SHEET_ID,
   homeDataSheetId: process.env.REACT_APP_HOME_DATA_SHEET_ID,
   eventSheetId: process.env.REACT_APP_EVENTS_SHEET_ID,
+  aboutSheetId: process.env.REACT_APP_ABOUT_SHEET_ID,
 };
